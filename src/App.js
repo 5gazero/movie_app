12d54c88314f89d9f59bbb1da510ac151def2955
5gazero <kayo9410@gmail.com>
2022-02-08 00:35:52 +0900

    [feat]: button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App() {
+import Button from "./Button";
+
+const App = () => {
   return (
     <div>
       <h1>Movie App</h1>
+      <Button text={"Continue"} />
     </div>
   );
-}
+};
 
 export default App;
