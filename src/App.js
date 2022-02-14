b749cec14622ab5545393ba1834c233cf2141277
5gazero <kayo9410@gmail.com>
2022-02-14 22:59:38 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
 
-const App = () => {
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
   return (
     <div>
-      <h1>Movie App</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
-};
+}
 
 export default App;
